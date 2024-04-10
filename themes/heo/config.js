2932b00dba564b2fd96cd3767deaafe7d1b43c64
nannant666 <99261386+nannant666@users.noreply.github.com>
2024-04-10 14:22:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎使用小海绵api', url: 'https://api.nan.plus' },
+    { title: '欢迎使用小海绵合租面板', url: 'https://hzs.nan.plus' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'AI赋能',
+  HEO_HERO_TITLE_2: '无限可能',
+  HEO_HERO_TITLE_3: 'blog.nan.plus',
+  HEO_HERO_TITLE_4: '海绵宝宝免费api',
+  HEO_HERO_TITLE_5: 'claude上线 轻松使用claude',
+  HEO_HERO_TITLE_LINK: 'https://hz-api.nan.plus',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -38,11 +38,10 @@ const CONFIG = {
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://hzs.nan.plus', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
