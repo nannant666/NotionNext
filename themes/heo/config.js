f1ec2d4be05847530be348e14655ee4ca7d2111e
nannant666 <99261386+nannant666@users.noreply.github.com>
2024-04-12 02:23:56 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -36,12 +36,10 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🤖️ 数码科技爱好者', 
   ],
-  HEO_INFO_CARD_URL: 'https://hzs.nan.plus', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://blog.nan.plus', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -104,10 +102,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'Contact Me',
+  HEO_SOCIAL_CARD_TITLE_2: '',
+  HEO_SOCIAL_CARD_TITLE_3: '点击查看我的联系方式',
+  HEO_SOCIAL_CARD_URL: 'https://',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
